2ddba620de92708fc41eac3133adf62a67a342de
PewJay <justinjayhernando900@gmail.com>
2026-08-11 00:02:23 +0800

    maintenance.js

--- js/maintenance.js
+++ js/maintenance.js
@@ -13,7 +13,7 @@
    MAINTENANCE SWITCH
    ========================================================= */
 
-const MAINTENANCE_MODE = true;
+const MAINTENANCE_MODE = false;
 
 
 /* =========================================================
